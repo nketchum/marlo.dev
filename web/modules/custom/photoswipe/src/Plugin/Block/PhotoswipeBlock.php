b821eb0232c8d508ee8eb1ce8451fa4bccfb8499
Nicholas Ketchum <nketchum@gmail.com>
2016-08-04 16:57:22 -0400

    first commit.

---
+++ web/modules/custom/photoswipe/src/Plugin/Block/PhotoswipeBlock.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\photoswipe\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'PhotoswipeBlock' block.
+ *
+ * @Block(
+ *  id = "photoswipe",
+ *  admin_label = @Translation("PhotoSwipe"),
+ * )
+ */
+class PhotoswipeBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $build = [];
+    $build['photoswipe']['#markup'] = $this->content();
+
+    return $build;
+
+  }
+
+  private function content() {
+
+    $content = <<<EOT
+<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="pswp__bg"></div>
+  <div class="pswp__scroll-wrap">
+
+    <!-- Don't modify these 3 pswp__item elements, data is added later. -->
+    <div class="pswp__container">
+      <div class="pswp__item"></div>
+      <div class="pswp__item"></div>
+      <div class="pswp__item"></div>
+    </div>
+
+    <div class="pswp__ui pswp__ui--hidden">
+      <div class="pswp__top-bar">
+        <div class="pswp__counter"></div>
+        <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+        <button class="pswp__button pswp__button--share" title="Share"></button>
+        <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+        <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+        <div class="pswp__preloader">
+          <div class="pswp__preloader__icn">
+            <div class="pswp__preloader__cut">
+              <div class="pswp__preloader__donut"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+        <div class="pswp__share-tooltip"></div>
+      </div>
+      <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>
+      <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>
+      <div class="pswp__caption">
+        <div class="pswp__caption__center"></div>
+      </div>
+
+    </div>
+  </div>
+</div>
+EOT;
+
+    return $content;
+
+  }
+
+}
---
+++ web/themes/custom/minsk/webpack.config.js
@@ -0,0 +1,30 @@
+(function() {
+
+  'use strict'
+
+  var webpack = require('webpack')
+  var path = require('path')
+
+  module.exports = {
+    devtool: 'source-map',
+    entry: path.resolve(__dirname, './src/js/index.js'),
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: 'http://marlo.dev/',
+      filename: 'js/scripts.min.js'
+    },
+    plugins: [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        include: /\.min\.js$/,
+        mangle: true,
+        compress: { warnings: false }
+      }),
+      new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery'
+      })
+    ]
+  }
+
+})()
